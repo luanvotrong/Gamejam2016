e5f4505b698cbb484c852378894e8e4c87f1d176
luan.votrong <vtluan288@gmail.com>
2016-11-16 15:05:23 +0700

    - Update move

---
+++ Assets/Data/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private float MOVING_SPEED = 5;
+	public enum MOVING_STATE {
+		STILL = 0,
+		LEFT,
+		RIGHT
+	}
+	private MOVING_STATE m_movingState;
+
+	private Transform m_transform;
+
+	// Use this for initialization
+	void Start () {
+		m_transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		Vector3 pos = m_transform.position;
+
+		switch (m_movingState) {
+		case MOVING_STATE.STILL:
+			break;
+		case MOVING_STATE.LEFT:
+			pos.x -= MOVING_SPEED * Time.deltaTime;
+			break;
+		case MOVING_STATE.RIGHT:
+			pos.x += MOVING_SPEED * Time.deltaTime;
+			break;
+		}
+
+		m_transform.position = pos;
+	}
+
+	public void SetMovingState(MOVING_STATE state) {
+		m_movingState = state;
+	}
+
+	public MOVING_STATE GetMovingState() {
+		return m_movingState;
+	}
+}
---
+++ Assets/Data/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	Transform m_transform;
+	Player m_player;
+
+	// Use this for initialization
+	void Start () {
+		m_transform = GetComponent<Transform> ();
+		m_player = GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 touch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 pos = (m_transform.position);
+
+			if (touch.y < pos.y) {
+				if (touch.x < pos.x) {
+					m_player.SetMovingState (Player.MOVING_STATE.LEFT);
+				} else {
+					m_player.SetMovingState (Player.MOVING_STATE.RIGHT);
+				}
+			} else {
+				Debug.LogFormat("projectile");
+			}
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			if (m_player.GetMovingState () != Player.MOVING_STATE.STILL) {
+				m_player.SetMovingState (Player.MOVING_STATE.STILL);
+			}
+		}
+	}
+}
